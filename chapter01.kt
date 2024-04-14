5b82436779a1abe0646fee53df71bdf5fda719c4
o3ojunseok <o3ojunseok@gmail.com>
2024-04-14 23:13:38 +0900

    Feat: chapter01

--- chapter01.kt
+++ chapter01.kt
@@ -1,19 +1,25 @@
-data class Person(
-    val name: String,
-    val age: Int? = null // 널이 될 수 있는 타입과 파라미터 디폴트 값
-)
-
-// 최상위 함수
 fun main(args: Array<String>) {
-    val persons = listOf(
-        Person("Alice"),
-        Person("Bob", age = 29) // 이름 붙은 파라미터
-    )
-
-    val oldest = persons.maxBy { it.age ?: 0 } // 람다 식과 엘비스 연산자
-    println("The oldest is: $oldest") // 문자열 템플릿
+  println("Hello, world!")
 }
 
+fun max(a: Int, b: Int): Int {
+  return if (a > b) a else b
+}
 
-val s: String? = null // 널이 될 수 있음
-var s2: String = "" // 널이 될 수 없음
+fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+val answer = 42 // 타입 생략
+val answer: Int = 42 // 타입 지정
+
+val answer: Int
+answer = 42
+
+val message: String
+if (canPerformOperation()) {
+		message = "Success"
+		// ... 연산을 수행한다. 
+} else {
+		message = "Failed"
+}
